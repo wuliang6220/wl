10ff434b62041504032b723f818c8a0ca528f657
wishchina <wishchina@wishchinadeMacBook-Air.local>
2017-09-14 13:50:11 +0800

    Initial Commit

---
+++ hello world/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hello world
+//
+//  Created by wishchina on 2017/9/14.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
